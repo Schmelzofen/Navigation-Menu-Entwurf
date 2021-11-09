272bc024aef58fdb213fb6fcc11a721247ffd083
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-09 12:31:46 +0100

    Navigation Menu Entwurf

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let burgerMenuContainer = document.getElementById("burgerMenuContainer")
+let headerContainer = document.getElementById("headerContainer")
+
+function toggleMenu(){
+    burgerMenuContainer.classList.toggle("open")
+    headerContainer.classList.toggle("toggle")
+}
